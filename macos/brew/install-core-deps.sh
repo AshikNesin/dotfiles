f2703c059800e7c00b3d452202bb5f7b36a71782
Ashik Nesin <mail@ashiknesin.com>
2025-12-12 09:29:21 -0800

    clean up apps

--- macos/brew/install-core-deps.sh
+++ macos/brew/install-core-deps.sh
@@ -7,17 +7,14 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Installs Casks
-brew tap homebrew/cask
-
 
 brew install 1password
 brew install 1password-cli
-brew install cursor
+# brew install cursor
 brew install discord
 brew install google-chrome #Chrome
-brew install homebrew/cask-versions/firefox-nightly # Firefox Nightly
-brew install homebrew/cask-versions/google-chrome-canary # Chrome Canary
+brew install firefox-nightly # Firefox Nightly
+# brew install google-chrome-canary # Chrome Canary
 # brew install keycastr
 # brew install lunar - display brightness
 brew install raycast
