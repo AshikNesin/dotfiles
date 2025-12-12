c9468b7ad5f3802d0bf2277d5b5687594f06f99c
Ashik Nesin <mail@ashiknesin.com>
2025-12-12 11:11:50 -0800

    Add screenshots and update MAS install script.

--- macos/mas-install.sh
+++ macos/mas-install.sh
@@ -5,11 +5,10 @@ mas install 1006087419 # SnippetsLab - https://apps.apple.com/in/app/snippetslab
 mas install 1294126402 # HEIC Converter - https://apps.apple.com/in/app/heic-converter/id1294126402
 mas install 966085870  # TickTick - To-Do List & Calendar - https://apps.apple.com/in/app/ticktick-to-do-list-calendar/id966085870
 # mas install 1289197285 # MindNode - Mind Map & Outline - https://apps.apple.com/in/app/mindnode-mind-map-outline/id1289197285
-mas install 1513574319 # Glance - Quick Look Plugin - https://apps.apple.com/in/app/glance-quick-look-plugin/id1513574319
 mas install 490461369  # Bandwidth - https://apps.apple.com/in/app/bandwidth/id490461369
 mas install 1470584107 # Dato - https://apps.apple.com/in/app/dato/id1470584107
 mas install 1263070803 # Lungo - https://apps.apple.com/in/app/lungo/id1263070803
 mas install 931571202  # QuickShade - https://apps.apple.com/in/app/quickshade/id931571202
 mas install 1611378436 # Pure Paste - https://apps.apple.com/in/app/pure-paste/id1611378436
-# mas install 497799835  # Xcode - https://apps.apple.com/in/app/xcode/id497799835
+mas install 497799835  # Xcode - https://apps.apple.com/in/app/xcode/id497799835
 # mas install 639968404  # Parcel - Delivery Tracking - https://apps.apple.com/in/app/parcel-delivery-tracking/id639968404
